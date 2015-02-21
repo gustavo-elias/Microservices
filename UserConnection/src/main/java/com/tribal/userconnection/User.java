d9972782dd94efd16f7c382e96bc01d89375c89b
tavo <tavo.elias@gmail.com>
2015-02-21 12:16:40 +0000

    Covering edge cases

--- UserConnection/src/main/java/com/tribal/userconnection/User.java
+++ UserConnection/src/main/java/com/tribal/userconnection/User.java
@@ -2,12 +2,16 @@ package com.tribal.userconnection;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class User {
 
+	private final static Logger LOGGER = Logger.getLogger(User.class.getName());
+	
 	private String name;
 	private int id;
-	List<User> usersConnectedTo = new ArrayList<User>();
+	private List<User> usersConnectedTo = new ArrayList<User>();
 	
 	public User(String name, int id) {
 		this.name = name;
@@ -15,7 +19,17 @@ public class User {
 	}
 	
 	public void connectToUser(User userToConnectTo) {
-		usersConnectedTo.add(userToConnectTo);
+		if(null != userToConnectTo) 
+		{
+			if (!usersConnectedTo.contains(userToConnectTo)) {
+				usersConnectedTo.add(userToConnectTo);
+				userToConnectTo.connectToUser(this);
+			} else {
+				LOGGER.log(Level.INFO, "Users " + this.name + ", " + userToConnectTo.getName() + " were already connected");
+			}
+		} else {
+			LOGGER.log(Level.INFO, "User " + this.name +" can't connect to a null user");
+		}
 	}
 
 	public List<User> retrieveUsersConnected() {
--- UserConnection/src/test/java/com/tribal/userconnection/UserTest.java
+++ UserConnection/src/test/java/com/tribal/userconnection/UserTest.java
@@ -11,24 +11,45 @@ public class UserTest {
 	private static final int USERNAME_ID_ONE = 1;
 	private static final String USERNAME_TWO = "User2";
 	private static final int USERNAME_ID_TWO = 2;
-	private User user, user2;
+	private User user1, user2;
 	
 	@Before
 	public void setUp() {
-		user = new User(USERNAME_ONE, USERNAME_ID_ONE);
+		user1 = new User(USERNAME_ONE, USERNAME_ID_ONE);
 		user2 = new User(USERNAME_TWO, USERNAME_ID_TWO);
 	}
 	
 	@Test
-	public void connectingToAnUserShouldAddUserToConnections() {
-		user.connectToUser(user2);
-		
-		assertEquals(1, user.retrieveUsersConnected().size());
-		assertEquals(user2, user.retrieveUsersConnected().get(0));
+	public void newlyCreatedUserShouldHaveEmptyListOfConnections() {
+		assertEquals(0, user1.retrieveUsersConnected().size());
 	}
 	
 	@Test
-	public void newlyCreatedUserShouldHaveEmptyListOfConnections() {
-		assertEquals(0, user.retrieveUsersConnected().size());
+	public void connectingToNullUserShouldNotAddConnections() {
+		user1.connectToUser(null);
+		
+		assertEquals(0, user1.retrieveUsersConnected().size());
 	}
+	
+	@Test
+	public void alreadyConnectedUsersShouldNotBeAddedTwice() {
+		user1.connectToUser(user2);
+		user2.connectToUser(user1);
+		
+		assertEquals(1, user1.retrieveUsersConnected().size());
+		assertEquals(1, user2.retrieveUsersConnected().size());
+	}
+	
+	@Test
+	public void connectingToAnUserShouldAddUserToConnectionsForBothUsers() {
+		user1.connectToUser(user2);
+		
+		assertEquals(1, user1.retrieveUsersConnected().size());
+		assertEquals(user2, user1.retrieveUsersConnected().get(0));
+		
+		assertEquals(1, user2.retrieveUsersConnected().size());
+		assertEquals(user1, user2.retrieveUsersConnected().get(0));
+	}
+	
+
 }
